dc0325c5138484b2e45c6db92e543fc39595713c
backslash112 <yangcun@live.com>
2015-09-11 13:10:03 +0800

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-YCRangeSlider_Tests/Pods-YCRangeSlider_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_YCRangeSlider_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_YCRangeSlider_TestsVersionString[];
+
---
+++ Example/Pods/Target Support Files/YCRangeSlider/YCRangeSlider-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YCRangeSlider : NSObject
+@end
+@implementation PodsDummy_YCRangeSlider
+@end
